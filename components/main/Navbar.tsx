9070ea379e7a225afb45fcbf22e496fb4bf66f5e
welcomecurry <bhandalamit@gmail.com>
2024-01-14 16:48:20 -0700

    fix nav image

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
-            src="/prOFphet.png"
+            src="./prOFphet.png"
             alt="logo"
             width={45}
             height={45}
